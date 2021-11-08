6ce84d8fc9e45004fe0c62859429714df53d84a7
elvisyong <elvisyong@gmail.com>
2021-11-08 10:22:56 +0800

    Add gifs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import './App.css';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+  'https://c.tenor.com/hIOlGyNoehEAAAAC/hologra-holo-graffiti.gif',
+  'https://c.tenor.com/7UX8spygc8wAAAAd/hololive-nakiri-ayame.gif',
+  'https://c.tenor.com/iQ-tmMIKsSkAAAAC/yo-dayo-nakiri.gif',
+  'https://c.tenor.com/ygNn73iaQ7wAAAAC/hololive-subaru.gif',
+  'https://c.tenor.com/SAmNsaoO3HEAAAAC/hololive-%E3%83%9B%E3%83%AD%E3%83%A9%E3%82%A4%E3%83%96.gif',
+  'https://c.tenor.com/-ONQYN540p8AAAAd/%E7%99%BE%E9%AC%BC%E3%81%82%E3%82%84%E3%82%81-%E3%81%82%E3%82%84%E3%82%81.gif'
+]
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -64,6 +72,18 @@ const App = () => {
     </button>
   )
 
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <div className="gif-grid">
+        {TEST_GIFS.map(gif => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   /*
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
@@ -79,11 +99,12 @@ const App = () => {
       <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="container">
           <div className="header-container">
-            <p className="header">🖼 GIF Portal</p>
+            <p className="header">Ayame GIF Portal</p>
             <p className="sub-text">
-              View your GIF collection in the metaverse ✨
+              View your Ayame GIF collection in the metaverse ✨
             </p>
             {!walletAddress && renderNotConnectedContainer()}
+            {walletAddress && renderConnectedContainer()}
           </div>
           <div className="footer-container">
             <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
